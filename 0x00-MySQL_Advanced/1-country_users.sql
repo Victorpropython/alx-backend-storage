db86504c8b6716c29124d75e14c2382fbaafd128
victorpropython <victorchibuzor3109@gmail.com>
2024-10-17 12:39:56 +0100

    for task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,10 @@
 -- An SQL script that creates a table users following 
 -- these requirements: id, email, name, country and!
 
+
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
-	country VARCHAR(2)
-	);
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
