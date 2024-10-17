88f089ce4fcb8d1e04d8561b3c3b70429b56efc9
victorpropython <victorchibuzor3109@gmail.com>
2024-10-17 10:57:59 +0100

    for task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
 -- An Sql script that craets a table users with the following requirements
 -- Id, email, name
+
 CREATE TABLE users (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
