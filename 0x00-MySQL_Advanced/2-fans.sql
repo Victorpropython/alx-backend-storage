4e7f00a895d0575ba2cc31a72b6387dda3d9b4a3
victorpropython <victorchibuzor3109@gmail.com>
2024-10-17 22:09:56 +0100

    for task 2

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- A script to rank country of bands, ordered by the number of
+-- (non unique fan) and it will be imported from a dump
+
+SELECT origin, SUM(fans) AS nb_fans
+FROM bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
