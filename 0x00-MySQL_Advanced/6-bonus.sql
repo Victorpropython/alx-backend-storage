9414f6152089c23d94b88009dcbfd7fd56e67183
victorpropython <victorchibuzor3109@gmail.com>
2024-10-18 17:36:29 +0100

    for task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,15 +1,21 @@
 -- An SQLscript that creates a stored procedure AddBonus 
 -- that adds a new correction for a student.
+
 DELIMITER $$
+
 CREATE PROCEDURE AddBonus(
 	IN user_id INT ,
 	IN project_name VARCHAR(255),
-	IN score INT)
+	IN score DECIMAL(5,2)
+)
 BEGIN
-	DECLARE project_id INT;
-	--Checking if project exists
-	SELECT id INTO project_id FROM projects 
-	WHERE name = project_name;
+	DECLARE project_id INT DEFAULT NULL;
+
+	
+	SELECT id INTO project_id
+	FROM projects 
+	WHERE name = project_name
+	LIMIT 1;
 
 	IF project_id IS NULL THEN
 		INSERT INTO projects (name) VALUES (project_name);
