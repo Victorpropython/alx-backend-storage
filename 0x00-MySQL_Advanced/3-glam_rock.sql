fcee9c521caac6acb0ebe7e2f9a17ef6471069d6
victorpropython <victorchibuzor3109@gmail.com>
2024-10-18 17:14:36 +0100

    for task 6

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,14 +1,21 @@
 -- An SQL script that lists all bands with Glam rock as their main style
 -- Using attributes formed and spilit for computing the lifespan
 
+DELIMITER $$
+
+CREATE FUNCTION calculate_lifespan(formed INT, split INT)
+RETURNS INT DETERMINISTIC
+BEGIN
+	DECLARE lifespan INT;
+	SET lifespan = IFNULL(split,  2022) - formed;
+	RETURN lifespan;
+END;
+
+DELIMITER;
+
+
 SELECT band_name,
-	CASE 
-		WHEN split IS NOT NULL THEN (split - formed)
-		ELSE (2022 - formed)
-	END AS lifespan
-FROM
-	metal_bands
-WHERE
-	style = 'Glam rock'
-ORDER BY
-	lifespan DESC;
+calculate_lifespan(formed,split) AS longevity
+FROM bands
+WHERE main_style = 'Glam rock'
+ORDER BY longevity DESC;
