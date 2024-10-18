a6fc467bf7d7f94ba9a6bdbb29e6c7157a5c5687
victorpropython <victorchibuzor3109@gmail.com>
2024-10-18 03:09:13 +0100

    for task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,14 @@
+-- An SQL script that lists all bands with Glam rock as their main style
+-- Using attributes formed and spilit for computing the lifespan
+
+SELECT band_name,
+	CASE 
+		WHEN split is NOT NULL THEN (split - formed)
+		ELSE (2022 - formed)
+	END AS lifespan
+FROM
+	metal_bands
+WHERE
+	style = 'Glam rock'
+ORDER BY
+	lifespan DESC;
