8091c1fd05ef08f7abe35d9037094061523900f7
victorpropython <victorchibuzor3109@gmail.com>
2024-10-18 18:40:58 +0100

    for task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,4 +2,25 @@
 -- that computes and store the average score for a student.
 -- note An average score can be a decimal
 
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	DECLARE avg_score DECIMAL(5, 2);
 
+	SELECT AVG(score) INTO avg_score
+	FROM corrections
+	WHERE user_id = user_id;
+
+	IF NOT EXISTS CREATE (SELECT 1 FROM average_score WHERE user_id = user_id) THEN
+
+		UPDATE average_score
+		SET avg_score = avg_score
+		WHERE user_id = user_id;
+	ELSE
+		INSERT INTO average_score (user_id, avg_score)
+		VALUES (user_id, avg_score);
+	END IF;
+
+END $$
+
+DELIMITER;
