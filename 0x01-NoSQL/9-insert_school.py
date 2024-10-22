368540664d4b2dbd3231b93fa431a5678f99c83f
victorpropython <victorchibuzor3109@gmail.com>
2024-10-22 18:13:43 +0100

    for task 8

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import pymongo
+
+"""Write a Python function that inserts a new document
+in a collection based on kwargs"""
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document into a MongoDB collection based on
+    Keyword
+
+    Args:
+        mongo_collection: The pymongo collection object.
+        **kwargs: keyword orguments representing the document fields and 
+    Returns:
+        the _id of the newly inserted document.
+    """
+    try:
+        results = mongo_collection.insertOne(kwargs)
+        return result.inserted_id
+    except Exception as e:
+        print(f"Error inserting document: {e}")
+        return None
