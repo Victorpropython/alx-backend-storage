77bf8dfcf8c80481d3682c0aa8a99d5746f051c2
victorpropython <victorchibuzor3109@gmail.com>
2024-10-24 04:44:04 +0100

    fot task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,3 +21,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    # Gteating a get function 
+    def get(self, key: str, fn: Callable None) -> Union[str, bytes, int, float, None]:
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+    
+    def get_str(self. key: str) -> str:
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, int)
